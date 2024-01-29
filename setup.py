6b2ea0acd1b4a6efcf9bd8368f4d166259bb2011
Preyansh Yadav <preyanshyadav234sony@gmail.com>
2024-01-29 22:47:08 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import find_packages,setup
 from typing import List
 
+HYPEN_E_DOT = '-e .'
 def get_requirements(file_path:str)->List[str]:
     """
     This function will return the list of requirements
     """
-print(get_requirements.__doc__)
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
 setup(
 name='mlproject',
 version='1.0',
